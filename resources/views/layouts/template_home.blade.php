033b863c1e4c5b69481dc9af92371292f8769c6b
phamminhkim <104416057+phamminhkim@users.noreply.github.com>
2023-09-07 10:04:48 +0700

    Chỉnh sửa lại login thành công

--- resources/views/layouts/template_home.blade.php
+++ resources/views/layouts/template_home.blade.php
@@ -115,8 +115,29 @@
             <div class="">
                 @if (Route::has('login'))
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                        <a href="{{ url('/setting') }}">Setting</a>
+                        <ul  class="navbar-nav text-center font-weight-bold">
+                            <li  class="nav-item">
+                                <a class="nav-link" href="{{ url('/setting') }}">Admin</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+    
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+    
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        </ul>
                     @else
                         <a href="{{ route('login') }}">Đăng nhập</a>
                         @if (Route::has('register'))
