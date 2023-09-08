85a640c02fb69d1e9b3f5c179798273ed5332254
tuhoang99 <tuhoang.12129@gmail.com>
2023-09-08 14:41:47 +0700

    [Tu] reponsive table

--- resources/js/components/BuySelected.vue
+++ resources/js/components/BuySelected.vue
@@ -33,44 +33,53 @@
           </div>
         </div>
       </div>
-      <table class="table hover border-0">
-        <thead>
-          <tr style="font-weight: 600; background: rgba(116, 155, 194, .15);">
-            <td>Hình ảnh</td>
-            <td>Sản phẩm</td>
-            <td>Đơn giá</td>
-            <td>Số lượng</td>
-            <td>Thành tiền</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(buy, index) in buy_selecteds" :key="index"
-            :style="{ background: index % 2 !== 0 ? 'rgba(238, 238, 238, .35)' : 'rgba(240, 240, 240, .75)' }">
-            <td v-if="buy.product_id" class=" d-flex justify-content-center">
-              <div class="thumbnail-image" v-if="buy.product.images.length > 0" v-for="image in buy.product.images">
-                <img :src="'../' + image.path" width="100%" height="100%" />
-              </div>
-            </td>
-            <td v-if="buy.product_id"> {{ buy.product.name }} </td>
-            <td v-if="buy.product_id"> {{ buy.product.price }} </td>
-            <td> {{ buy.quantity }} </td>
-            <td class="text-danger font-italic" style="font-size: 1rem;"> {{ buy.total_price }} đ</td>
-          </tr>
-          <tr style="background: rgba(116, 155, 194, .15);">
+      <div class="table-responsive">
+        <table class="table hover border-0">
+          <thead>
+            <tr style="font-weight: 600; background: rgba(116, 155, 194, .15);">
+              <td>Hình ảnh</td>
+              <td>Sản phẩm</td>
+              <td>Đơn giá</td>
+              <td>Số lượng</td>
+              <td>Thành tiền</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(buy, index) in buy_selecteds" :key="index"
+              :style="{ background: index % 2 !== 0 ? 'rgba(238, 238, 238, .35)' : 'rgba(240, 240, 240, .75)' }">
+              <td v-if="buy.product_id" class=" d-flex justify-content-center">
+                <div class="thumbnail-image" v-if="buy.product.images.length > 0" v-for="image in buy.product.images">
+                  <img :src="'../' + image.path" width="100%" height="100%" />
+                </div>
+              </td>
+              <td v-if="buy.product_id"> {{ buy.product.name }} </td>
+              <td v-if="buy.product_id"> {{ buy.product.price }} </td>
+              <td> {{ buy.quantity }} </td>
+              <td class="text-danger font-italic" style="font-size: 1rem;"> {{ buy.total_price }} đ</td>
+            </tr>
+            <!-- <tr style="background: rgba(116, 155, 194, .15);">
             <td></td>
             <td></td>
             <td></td>
-            <td></td>
-            <td class="font-weight-light font-italic" style="color: gray; font-size: .9rem;">Tổng số tiền ({{
-              buy_selecteds.length }} sản
+            <td></td> 
+            <td class="font-weight-light font-italic txt-total" style="color: gray; font-size: .9rem;">Tổng số
+              tiền ({{
+                buy_selecteds.length }} sản
               phẩm):
               <span class="text-danger font-weight-bold" style="font-size: 1rem;"> {{ total_price }} đ </span>
             </td>
-          </tr>
-        </tbody>
-      </table>
-      <div class="form-group text-right">
-        <button class="btn btn-warning py-2 px-4 font-weight-bold" style="color: white;">Đặt hàng</button>
+          </tr> -->
+          </tbody>
+        </table>
+      </div>
+      <div class="form-group">
+        <span class="font-weight-light font-italic txt-total" style="color: gray; font-size: .9rem;">Tổng số
+          tiền ({{
+            buy_selecteds.length }} sản
+          phẩm):
+          <span class="text-danger font-weight-bold" style="font-size: 1rem;"> {{ total_price }} đ </span>
+        </span>
+        <button class="btn btn-warning py-2 px-4 font-weight-bold float-right" style="color: white;">Đặt hàng</button>
       </div>
     </div>
   </div>
@@ -123,5 +132,17 @@ td {
   background: rgba(221, 230, 237, .25);
   border-radius: 10px;
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+}
+
+.txt-total {
+  line-height: 45px;
+
+  @media screen and (max-width: 767px) {
+    font-size: .65rem !important;
+
+    span {
+      font-size: .85rem !important;
+    }
+  }
 }
 </style>
